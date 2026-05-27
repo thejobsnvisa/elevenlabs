f552b4e410b794ff79e76796aa7ca606ce19a2bb
USER-28 <USER-28@growmore.local>
2026-05-27 15:30:26 +0530

    intial commit

--- api/index.js
+++ api/index.js
@@ -30,42 +30,16 @@ if (!SHEET_ID) {
 /* ===========================
    GOOGLE SERVICE ACCOUNT
 =========================== */
+const credentials = JSON.parse(
+  process.env.GOOGLE_SERVICE_ACCOUNT
+);
 
-const serviceAccountPath =
-  path.join(
-    "/tmp",
-    "service-account.json"
-  );
-
-const serviceAccountEnv =
-  process.env.GOOGLE_SERVICE_ACCOUNT;
-
-if (!serviceAccountEnv) {
-  throw new Error(
-    "GOOGLE_SERVICE_ACCOUNT env missing"
-  );
-}
-
-if (
-  !fs.existsSync(
-    serviceAccountPath
-  )
-) {
-  fs.writeFileSync(
-    serviceAccountPath,
-    serviceAccountEnv
-  );
-}
-
-const auth =
-  new google.auth.GoogleAuth({
-    keyFile:
-      serviceAccountPath,
-    scopes: [
-      "https://www.googleapis.com/auth/spreadsheets",
-    ],
-  });
-
+const auth = new google.auth.GoogleAuth({
+  credentials,
+  scopes: [
+    "https://www.googleapis.com/auth/spreadsheets",
+  ],
+});
 /* ===========================
    EXTRACT DATA
 =========================== */
